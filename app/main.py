874d82e7cf4acfea2cdb88fe8c8d0ec8765c8862
AaronNierras <167047239+AaronNierras@users.noreply.github.com>
2024-04-24 17:08:28 +0800

    added CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 # from fastapi.params import Body
 
 from app import models
@@ -8,6 +9,21 @@ from app.config import settings
 # models.Base.metadata.create_all(bind=engine) # generate database without alembic
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # ------------------- INIT ------------------------
 
